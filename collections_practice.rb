fe0402d2ba2a007e77061bdf8c3a110e9e8e3280
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-21 00:04:48 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,7 +1,9 @@
-sort_array_asc(nums)
+sort_array_asc(numbers)
 sort_array = []
-sort_array.collect do |nums|
+sort_array.collect do |numbers|
   sort_array << sort_array.sort
 end
 sort_array
 end
+
+sort_array_desc(numbers)
